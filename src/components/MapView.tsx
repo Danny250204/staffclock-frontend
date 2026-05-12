35558fe681b0b3e93765c3dc2642d61f3a5599f7
Danny250204 <danieligwama@gmail.com>
2026-05-12 15:11:21 +0100

    fix marker icon imports

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,11 +1,10 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
+// Fix the default marker icon without importing PNGs directly
 export const DefaultIcon = L.icon({
-  iconUrl: icon,
-  shadowUrl: iconShadow,
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
 });
